a73ef6ea9d20f0217904897c497977392d2a7ffd
verbeeckba <verbeeckba@aocws123.vlm.be>
2015-05-13 08:50:35 +0200

    Oplossen time slider reis in de tijd en toevoegen test app

---
+++ src/main/js/apps/test/nls/nl/bundle.js
@@ -0,0 +1,19 @@
+define(
+    ({
+
+        search: {
+            ui: {
+                placeHolder: "Adres, gemeente",
+                noValue: "Geen waarde",
+                searchTypes: {GEOLOCATOR: "adres"},
+                noResult: "Niet gevonden. Probeer opnieuw."
+            },
+            defaultInfoTemplate: {title: "Zoekresultaat", content: "${title}"}
+        },
+
+        splashscreen: {
+            loadTitle: "De kaart wordt geladen.",
+            loadBundle: "{percent}%"
+        }
+    })
+);
